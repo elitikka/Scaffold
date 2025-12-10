a2ff025394fc5b7e44fdc47979b14b7bed3b6880
elitikka <elin.tikkanen@gmail.com>
2025-12-10 14:40:32 +0200

    first commit

---
+++ app/src/main/java/fi/oamk/scaffold/MainActivity.kt
@@ -0,0 +1,106 @@
+package fi.oamk.scaffold
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import fi.oamk.scaffold.screens.ScaffoldApp
+import fi.oamk.scaffold.ui.theme.ScaffoldTheme
+
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ScaffoldTheme {
+                ScaffoldApp()
+                /*(
+                    topBar = {
+                        var expanded by remember { mutableStateOf(false)}
+                        TopAppBar (
+                            colors = TopAppBarDefaults.topAppBarColors (
+                                containerColor = MaterialTheme.colorScheme.primary,
+                                titleContentColor = MaterialTheme.colorScheme.primary,
+                            ),
+                            title = {
+                                Text("My app")
+                            },
+                            navigationIcon = {
+                                IconButton(onClick = { /* Do something */}) {
+                                  Icon(
+                                        imageVector = Icons.Filled.Menu,
+                                        contentDescription = "Localized description"
+                                    )
+                                }
+                            },
+                            actions = {
+                                IconButton(onClick = { expanded = !expanded }) {
+                                    Icon(
+                                        imageVector = Icons.Filled.MoreVert,
+                                        contentDescription = "Localized description"
+                                    )
+                                }
+                                DropdownMenu (
+                                    expanded = expanded,
+                                    onDismissRequest = { expanded = false }
+                                ) {
+                                    DropdownMenuItem (
+                                        text = { Text("Info")},
+                                        onClick = {/*DO SOMETHING*/}
+                                    )
+                                    DropdownMenuItem (
+                                        text = { Text("Settings")},
+                                        onClick = {/*DO SOMETHING*/}
+                                    )
+                                }
+                            },
+                        )
+                    },
+                ) { innerPadding ->
+                    Greeting("Android", modifier = Modifier.padding(innerPadding))
+                }*/
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ScaffoldTheme {
+        Greeting("Android")
+    }
+}
